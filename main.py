07c62adb64968d2612e4a78ca88d6762fdb5aff8
Ilyosbekdsgn <140714881+Ilyosbekdsgn@users.noreply.github.com>
2026-04-30 22:37:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = int(os.getenv("ADMIN_ID", 551853004))
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_STRING = os.getenv("SESSION_STRING")
